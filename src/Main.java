c540e54305985ecd6e839396fa136734d2ab7ccd
LuanLucasFS <luanlucasfs17@gmail.com>
2024-10-05 20:48:34 -0300

    Create CRUD

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
+import java.sql.Connection;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        DbFunctions db = new DbFunctions();
+        Connection conn = db.connect_to_db("CRUDJava", "postgres", "dbadmin");
+        //db.createTable(conn, "employee");
+        //db.insert_row(conn, "employee", "Paul", "Greek");
+        //db.update_name(conn, "employee", "John", "Joao");
+        db.delete_row_by_name(conn,"employee", "Joao");
+        db.read_data(conn, "employee");
+        //db.search_by_name(conn,"employee", "Paul");
     }
 }
